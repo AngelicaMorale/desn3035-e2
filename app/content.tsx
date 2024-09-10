e97af8120eb054883d0bd6f3c2d4cd95760b746f
AngelicaMorale <101543485@georgebrown.ca>
2024-09-10 18:06:07 +0000

    Last update

--- app/content.tsx
+++ app/content.tsx
@@ -1,10 +1,76 @@
 "use client"
 
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css"
+import ReactPlayer from 'react-player'
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
+import "leaflet/dist/leaflet.css"
+
+const data = [
+];
 
 export default function Content() {
 
     return <>
-        <h1>Hello, Next.js!</h1>
+
+        <div className="container">
+
+            <h1>Hello world</h1>
+
+            <div className="row">
+
+                <div className="col-6">
+                    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                        <TileLayer
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[51.505, -0.09]}>
+                            <Popup>
+                                A pretty CSS3 popup. <br /> Easily customizable.
+                            </Popup>
+                        </Marker>
+                    </MapContainer>
+                </div>
+
+
+
+                <div className="col-6">
+                    <ResponsiveContainer width="100%" height="100%">
+                        <LineChart
+                            width={500}
+                            height={300}
+                            data={data}
+                            margin={{
+                                top: 5,
+                                right: 30,
+                                left: 20,
+                                bottom: 5,
+                            }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                        </LineChart>
+                    </ResponsiveContainer>
+                </div>
+
+                <div className="col-12">
+                    <ReactPlayer width="100%" url='https://www.youtube.com/watch?v=LXb3EKWsInQ' />
+                </div>
+
+            </div>
+
+        </div>
+
+
+
+
+
     </>
 }
